bfdb0714399a2a31a2adefe41b0c61021bda7042
walter-wf <walter.franz@gmail.com>
2023-09-07 14:48:20 -0300

    se incluye date, math y condicionales

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let carrito = [];
+const productosDisponibles = [ //Array de Objetos.
+    { nombre: "Brandon Sanderson", precio: 10 },
+    { nombre: "Mass Effect", precio: 15 },
+    { nombre: "Steven Erikson", precio: 20 }
+];
 
-function checkAvailability() {
+function checkAvailability() { //Condicional
     const nombreProducto = prompt("Ingresa el nombre del producto que buscas");
 
-    switch (nombreProducto) {
-        case "1":
-        case "2":
-        case "3":
-            alert(`${nombreProducto} Lo tenemos!`);
-            break;
-        default:
-            alert(`${nombreProducto} Por el momento, está agotadísimo`);
+    const productoEncontrado = productosDisponibles.find(producto => producto.nombre === nombreProducto);
+
+    if (productoEncontrado) {
+        alert(`${nombreProducto} Lo tenemos! Precio: $${productoEncontrado.precio}`);
+    } else {
+        alert(`${nombreProducto} Por el momento, está agotadísimo`);
     }
 }
 
@@ -19,7 +22,7 @@ function calcularTotal() {
     const cantidad = parseInt(prompt("Ingresa la cantidad"));
 
     if (isNaN(precio) || isNaN(cantidad)) {
-        alert("Ingresa valores numéricos válidos para precio y cantidad.");
+        alert("Ingresa precio y cantidad válidos.");
         return;
     }
 
@@ -30,10 +33,16 @@ function calcularTotal() {
 function agregarAlCarro() {
     let agregarProducto = true;
 
-    while (agregarProducto) {
+    while (agregarProducto) { //Inclui un bucle para que el usuario pueda agregar productos hasta que decida no mas. 
         const nombreProducto = prompt("¿Qué producto quieres agregar al carrito?");
-        carrito.push(nombreProducto);
-        console.log(`${nombreProducto} Listo! Agregado al carrito!`);
+        const productoEncontrado = productosDisponibles.find(producto => producto.nombre === nombreProducto);
+
+        if (productoEncontrado) {
+            carrito.push(productoEncontrado);
+            console.log(`${nombreProducto} Listo! Agregado al carrito!`);
+        } else {
+            console.log(`${nombreProducto} no está disponible.`);
+        }
 
         const confirmacion = prompt("¿Deseas agregar otro producto al carrito? (Sí/No)").toLowerCase();
         if (confirmacion !== "si") {
@@ -42,8 +51,25 @@ function agregarAlCarro() {
     }
 }
 
-function displayCart() {
-    console.log("Carrito de Compras:", carrito);
-    alert(`Carrito de Compras: ${carrito.join(', ')}`);
+function displayCart() { 
+    console.log("Carrito de Compras:");
+    carrito.forEach(producto => {
+        console.log(`Nombre: ${producto.nombre}, Precio: $${producto.precio}`);
+    });
+
+    const totalCarrito = carrito.reduce((total, producto) => total + producto.precio, 0);
+    alert(`Carrito de Compras (Total: $${totalCarrito.toFixed(2)}):\n${carrito.map(producto => producto.nombre).join(', ')}`);
 }
 
+
+//  función Date y Math
+function obtenerFechaActual() {
+    const fecha = new Date();
+    const dia = fecha.getDate();
+    const mes = fecha.getMonth() + 1;
+    const año = fecha.getFullYear();
+    return `${dia}/${mes}/${año}`;
+}
+
+agregarAlCarro(); 
+console.log("Fecha actual:", obtenerFechaActual()); 
