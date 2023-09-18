be0fefd7854c48ebb02d836944e8be47949a1c74
walter-wf <walter.franz@gmail.com>
2023-09-18 19:39:29 -0300

    retiro alerts y prompts, reemplazo por salida html, falta css

--- script.js
+++ script.js
@@ -1,75 +1,56 @@
 let carrito = [];
-const productosDisponibles = [ //Array de Objetos.
-    { nombre: "Brandon Sanderson", precio: 10 },
-    { nombre: "Mass Effect", precio: 15 },
-    { nombre: "Steven Erikson", precio: 20 }
+const productosDisponibles = [
+    { nombre: "Brandon Sanderson", precio: 10, cantidad: 5 },
+    { nombre: "Mass Effect", precio: 15, cantidad: 3 },
+    { nombre: "Garden Of The Moons", precio: 20, cantidad: 7 }
 ];
 
-function checkAvailability() { //Condicional
+function checkAvailability() {
     const nombreProducto = prompt("Ingresa el nombre del producto que buscas");
-
     const productoEncontrado = productosDisponibles.find(producto => producto.nombre === nombreProducto);
 
     if (productoEncontrado) {
-        alert(`${nombreProducto} Lo tenemos! Precio: $${productoEncontrado.precio}`);
+        mostrarMensaje(`${nombreProducto} Lo tenemos! Precio: $${productoEncontrado.precio}`);
     } else {
-        alert(`${nombreProducto} Por el momento, está agotadísimo`);
+        mostrarMensaje(`${nombreProducto} Por el momento, está agotadísimo`);
+    }
+}
+
+function agregarAlCarro(nombreProducto) {
+    const productoEncontrado = productosDisponibles.find(producto => producto.nombre === nombreProducto);
+
+    if (productoEncontrado && productoEncontrado.cantidad > 0) {
+        carrito.push(productoEncontrado);
+        productoEncontrado.cantidad--; // Disminuir la cantidad disponible
+        mostrarMensaje(`${nombreProducto} Listo! Agregado al carrito!`);
+    } else {
+        mostrarMensaje(`${nombreProducto} no está disponible.`);
     }
 }
 
 function calcularTotal() {
-    const precio = parseFloat(prompt("Ingresa el precio"));
-    const cantidad = parseInt(prompt("Ingresa la cantidad"));
-
-    if (isNaN(precio) || isNaN(cantidad)) {
-        alert("Ingresa precio y cantidad válidos.");
-        return;
-    }
-
-    const total = precio * cantidad;
-    alert(`Precio Total: $${total.toFixed(2)}`);
-}
-
-function agregarAlCarro() {
-    let agregarProducto = true;
-
-    while (agregarProducto) { //Inclui un bucle para que el usuario pueda agregar productos hasta que decida no mas. 
-        const nombreProducto = prompt("¿Qué producto quieres agregar al carrito?");
-        const productoEncontrado = productosDisponibles.find(producto => producto.nombre === nombreProducto);
-
-        if (productoEncontrado) {
-            carrito.push(productoEncontrado);
-            console.log(`${nombreProducto} Listo! Agregado al carrito!`);
-        } else {
-            console.log(`${nombreProducto} no está disponible.`);
-        }
-
-        const confirmacion = prompt("¿Deseas agregar otro producto al carrito? (Sí/No)").toLowerCase();
-        if (confirmacion !== "si") {
-            agregarProducto = false;
-        }
-    }
-}
-
-function displayCart() { 
-    console.log("Carrito de Compras:");
-    carrito.forEach(producto => {
-        console.log(`Nombre: ${producto.nombre}, Precio: $${producto.precio}`);
-    });
-
     const totalCarrito = carrito.reduce((total, producto) => total + producto.precio, 0);
-    alert(`Carrito de Compras (Total: $${totalCarrito.toFixed(2)}):\n${carrito.map(producto => producto.nombre).join(', ')}`);
+    mostrarMensaje(`Total de la compra: $${totalCarrito.toFixed(2)}`);
 }
 
-
-//  función Date y Math
-function obtenerFechaActual() {
-    const fecha = new Date();
-    const dia = fecha.getDate();
-    const mes = fecha.getMonth() + 1;
-    const año = fecha.getFullYear();
-    return `${dia}/${mes}/${año}`;
+function mostrarCarrito() {
+    let carritoHTML = "<h2>Carrito de Compras:</h2>";
+    carrito.forEach(producto => {
+        carritoHTML += `<p>Nombre: ${producto.nombre}, Precio: $${producto.precio}</p>`;
+    });
+    mostrarMensaje(carritoHTML);
 }
 
-agregarAlCarro(); 
-console.log("Fecha actual:", obtenerFechaActual()); 
+function mostrarMensaje(mensaje) {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = mensaje;
+}
+
+// Event Listeners para botones
+document.getElementById('checkAvailabilityButton').addEventListener('click', checkAvailability);
+document.getElementById('addToCartButton').addEventListener('click', function () {
+    const nombreProducto = prompt("¿Qué producto quieres agregar al carrito?");
+    agregarAlCarro(nombreProducto);
+});
+document.getElementById('showCartButton').addEventListener('click', mostrarCarrito);
+document.getElementById('calculateTotalButton').addEventListener('click', calcularTotal);
