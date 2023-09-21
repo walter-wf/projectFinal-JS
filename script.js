6f04c82ccca97c284cfd178358c18e02b5afc441
walter-wf <walter.franz@gmail.com>
2023-09-21 18:06:07 -0300

    agrego tienda, json, funcionalidades

--- script.js
+++ script.js
@@ -1,56 +1,232 @@
 let carrito = [];
-const productosDisponibles = [
-    { nombre: "Brandon Sanderson", precio: 10, cantidad: 5 },
-    { nombre: "Mass Effect", precio: 15, cantidad: 3 },
-    { nombre: "Garden Of The Moons", precio: 20, cantidad: 7 }
-];
+let autoresCienciaFiccion = [] // Variable para almacenar los datos del JSON
 
-function checkAvailability() {
-    const nombreProducto = prompt("Ingresa el nombre del producto que buscas");
-    const productoEncontrado = productosDisponibles.find(producto => producto.nombre === nombreProducto);
+// Función para mostrar los elementos del JSON
+function mostrarElementos() {
+    // Tu código para mostrar elementos aquí
+}
 
-    if (productoEncontrado) {
-        mostrarMensaje(`${nombreProducto} Lo tenemos! Precio: $${productoEncontrado.precio}`);
+// Función para cargar los datos desde el JSON
+function cargarAutoresCienciaFiccion() {
+    fetch('productos.json')
+        .then(response => response.json())
+        .then(data => {
+            autoresCienciaFiccion = data;
+            mostrarElementos(); 
+        })
+        .catch(error => console.error('Error al cargar el JSON:', error));
+}
+
+// Llama a la función para cargar los datos desde el JSON
+cargarAutoresCienciaFiccion();
+
+
+
+
+// Función para agregar un autor al carrito
+function agregarAutorAlCarro(nombre) {
+    // Verifica que autoresCienciaFiccion y carrito estén definidos y contengan datos
+    if (typeof autoresCienciaFiccion === 'object' && Object.keys(autoresCienciaFiccion).length > 0) {
+        if (autoresCienciaFiccion.hasOwnProperty(nombre)) {
+            if (!carrito.hasOwnProperty(nombre)) {
+                carrito[nombre] = {
+                    precio: autoresCienciaFiccion[nombre].precio,
+                    cantidad: 0
+                };
+            }
+
+            if (autoresCienciaFiccion[nombre].cantidad > 0) {
+                carrito[nombre].cantidad++;
+                autoresCienciaFiccion[nombre].cantidad--;
+
+                Toastify({
+                    text: `Agregado ${nombre} al carrito`,
+                    duration: 2000,
+                    gravity: "top",
+                    position: "center",
+                    style: {
+                        background: "#007bff" 
+                    },
+                    stopOnFocus: true,
+                }).showToast();
+
+                // Mostrar mensaje en un elemento HTML con el id "mensaje"
+                document.getElementById('mensaje').innerText = `Agregado ${nombre} al carrito`;
+
+            } else {
+                Toastify({
+                    text: `${nombre} no está disponible`,
+                    duration: 2000,
+                    gravity: "top",
+                    position: "center",
+                    style: {
+                        backgroundColor: "#ff0000"
+                    },
+                    stopOnFocus: true,
+                }).showToast();
+
+                // Mostrar mensaje en un elemento HTML con el id "mensaje"
+                document.getElementById('mensaje').innerText = `${nombre} no está disponible.`;
+            }
+        } else {
+            Toastify({
+                text: `${nombre} no está en la lista de autores`,
+                duration: 2000,
+                gravity: "top",
+                position: "center",
+                style: {
+                    backgroundColor: "#ff0000"
+                },
+                stopOnFocus: true,
+            }).showToast();
+
+            /* // Mostrar mensaje en un elemento HTML con el id "mensaje"
+            document.getElementById('mensaje').innerText = `${nombre} no está en la lista de autores.`; */
+        }
     } else {
-        mostrarMensaje(`${nombreProducto} Por el momento, está agotadísimo`);
+        alert('Los datos de autoresCienciaFiccion no se han cargado correctamente.'); 
     }
 }
 
-function agregarAlCarro(nombreProducto) {
-    const productoEncontrado = productosDisponibles.find(producto => producto.nombre === nombreProducto);
 
-    if (productoEncontrado && productoEncontrado.cantidad > 0) {
-        carrito.push(productoEncontrado);
-        productoEncontrado.cantidad--; // Disminuir la cantidad disponible
-        mostrarMensaje(`${nombreProducto} Listo! Agregado al carrito!`);
-    } else {
-        mostrarMensaje(`${nombreProducto} no está disponible.`);
-    }
+function mostrarMensaje(Agregado) {
+    Toastify({
+        text: mensaje,
+        duration: 2000,
+        gravity: "top",
+        position: "center",
+        style: {
+            background: "#007bff"
+        },
+        stopOnFocus: true,
+    }).showToast();
 }
 
 function calcularTotal() {
-    const totalCarrito = carrito.reduce((total, producto) => total + producto.precio, 0);
+    let totalCarrito = 0;
+
+    for (const autor in carrito) {
+        totalCarrito += carrito[autor].precio * carrito[autor].cantidad;
+    }
+
+    Toastify({
+        text: `Total de la compra: $${totalCarrito.toFixed(2)}`,
+        duration: 2000,
+        gravity: "top",
+        position: "center",
+        style: {
+            background: "#007bff" 
+        },
+        stopOnFocus: true,
+    }).showToast();
+
     mostrarMensaje(`Total de la compra: $${totalCarrito.toFixed(2)}`);
 }
 
 function mostrarCarrito() {
-    let carritoHTML = "<h2>Carrito de Compras:</h2>";
-    carrito.forEach(producto => {
-        carritoHTML += `<p>Nombre: ${producto.nombre}, Precio: $${producto.precio}</p>`;
-    });
-    mostrarMensaje(carritoHTML);
+    let carritoHTML = "Carrito de Compras:";
+    
+    for (const autor in carrito) {
+        carritoHTML += ` ${autor}, $${(carrito[autor].precio * carrito[autor].cantidad).toFixed(2)}`;
+    }
+
+    Toastify({
+        text: carritoHTML,
+        duration: 0,
+        gravity: "top",
+        position: "center",
+        style: {
+            background: "#007bff" 
+        },
+        stopOnFocus: true,
+        close: true,
+    }).showToast();
 }
+
+
+                                                                                //Tienda//
+
+// Función para decrementar la cantidad
+function decrementQuantity(inputId) {
+    const quantityInput = document.getElementById(inputId);
+    let quantity = parseInt(quantityInput.value);
+    
+    if (quantity > 1) {
+        quantity--;
+        quantityInput.value = quantity;
+    }
+}
+
+// Función para incrementar la cantidad
+function incrementQuantity(inputId) {
+    const quantityInput = document.getElementById(inputId);
+    let quantity = parseInt(quantityInput.value);
+    
+    quantity++;
+    quantityInput.value = quantity;
+}
+
+
+
+function agregarAlCarrito(producto, precio) {
+  
+    if (carrito.hasOwnProperty(producto)) {
+        carrito[producto].cantidad++;
+    } else {
+        carrito[producto] = {
+            precio: precio,
+            cantidad: 1
+        };
+    }
+
+  
+    mostrarMensaje(`Agregado ${producto} al carrito`);
+}
+
 
 function mostrarMensaje(mensaje) {
-    const outputDiv = document.getElementById('output');
-    outputDiv.innerHTML = mensaje;
+   
+    Toastify({
+        text: mensaje,
+        duration: 2000,
+        gravity: "top",
+        position: "center",
+        style: {
+            background: "#007bff"
+        },
+        stopOnFocus: true,
+    }).showToast();
 }
 
-// Event Listeners para botones
-document.getElementById('checkAvailabilityButton').addEventListener('click', checkAvailability);
-document.getElementById('addToCartButton').addEventListener('click', function () {
-    const nombreProducto = prompt("¿Qué producto quieres agregar al carrito?");
-    agregarAlCarro(nombreProducto);
-});
-document.getElementById('showCartButton').addEventListener('click', mostrarCarrito);
-document.getElementById('calculateTotalButton').addEventListener('click', calcularTotal);
+
+
+
+// Función para mostrar los productos en el carrito
+function mostrarCarrito() {
+    const carritoContenido = document.getElementById('carritoContenido');
+    carritoContenido.innerHTML = '';
+
+    let totalCarrito = 0;
+
+    for (const producto in carrito) {
+        const { precio, cantidad } = carrito[producto];
+        const subtotal = precio * cantidad;
+        totalCarrito += subtotal;
+
+        const productoHTML = `
+            <div>
+                <p>${producto} - Cantidad: ${cantidad} - Precio: $${precio.toFixed(2)} - Subtotal: $${subtotal.toFixed(2)}</p>
+            </div>
+        `;
+
+        carritoContenido.innerHTML += productoHTML;
+    }
+
+    carritoContenido.innerHTML += `
+        <hr>
+        <p>Total de la compra: $${totalCarrito.toFixed(2)}</p>
+    `;
+
+    $('#carritoModal').modal('show'); 
+}
+
